97850603ae6580ed871fd7bcf716a78ddb5d17e6
PeculiarRP <119080618+PeculiarRP@users.noreply.github.com>
2023-04-07 23:37:53 +0300

    Все вроде порядок

--- Splav2/MainWindow.xaml.cs
+++ Splav2/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ProjectForModel
+namespace Splav2
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
